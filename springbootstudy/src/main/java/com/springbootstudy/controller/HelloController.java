98bf1cefe9f6898c9e93f2c6c5ef1aa4e8471aa1
junqing.yang <junqing.yang@ebaotech.com>
2021-01-14 10:26:41 +0800

    提交HelloController的新改動

--- springbootstudy/src/main/java/com/springbootstudy/controller/HelloController.java
+++ springbootstudy/src/main/java/com/springbootstudy/controller/HelloController.java
@@ -1,14 +1,19 @@
 package com.springbootstudy.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${book.name}")
+    private String bookName;
+
     @RequestMapping("/testHello")
     public String helloWorld(){
         System.out.println("這是一個helloWorld");
+        System.out.println("book name ="+bookName);
         return "這是一個helloWorld";
     }
 }
